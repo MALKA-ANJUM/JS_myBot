61179d31f086bea18688190293ded24637a3bf28
Malka Anjum <75417185+MALKA-ANJUM@users.noreply.github.com>
2022-04-24 21:23:32 +1300

    commit

---
+++ static/scripts/response.js
@@ -0,0 +1,9 @@
+function getHardResponse(input){
+    if(input=="hello"){
+        return  "hii there!"                                                 
+    }else if(input = "how was the day?"){
+        return "good enough"
+    }else if("bye"){
+        return "goodbye"
+    }
+}
